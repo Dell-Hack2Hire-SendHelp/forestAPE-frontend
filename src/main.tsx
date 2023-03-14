2acab77bd064270b79bf17281fb02464b2f2c653
AdmiJW <jamweg22@gmail.com>
2023-03-14 23:18:42 +0800

    JW: Added Redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,28 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import { Provider } from 'react-redux';
 import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 import { DialogContextProvider } from "./contexts/DialogContext";
 
 import AppMaterialTheme from "./styles/AppMaterialTheme";
-import App from "./App";
 import ViewTemplate from "./views/ViewTemplate";
+
 
 import "./styles/tailwind.css";
 import "@fontsource/lato";
 import "@fontsource/roboto";
 import '@fontsource/cantora-one';
 
+import App from "./App";
+import { store } from "./redux/store";
 
 const app = (
     <React.StrictMode>
+        {/* Redux Store */}
+        <Provider store={store}>
+        {/* Material UI Theme Context */}
         <ThemeProvider theme={AppMaterialTheme}>
         {/* Notistack Snackbar Provider */}
         <SnackbarProvider maxSnack={5} anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}>
@@ -29,6 +35,7 @@ const app = (
         </DialogContextProvider>
         </SnackbarProvider>
         </ThemeProvider>
+        </Provider>
     </React.StrictMode>
 );
 
